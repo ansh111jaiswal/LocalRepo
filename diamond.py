c2030202b80615a60586db1d2df9facf13c83228
ansh111jaiswal <ansh111jaiswal@gmail.com>
2026-07-23 22:18:12 +0530

    Diamond Pattern

---
+++ diamond.py
@@ -0,0 +1,17 @@
+n = 5
+
+# Upper half
+for i in range(n):
+    for j in range(n - i - 1):
+        print(" ", end="")
+    for k in range(2 * i + 1):
+        print("*", end="")
+    print()
+
+# Lower half
+for i in range(n - 2, -1, -1):
+    for j in range(n - i - 1):
+        print(" ", end="")
+    for k in range(2 * i + 1):
+        print("*", end="")
+    print()
